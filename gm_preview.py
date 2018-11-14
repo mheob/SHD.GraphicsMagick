c55b5a54794ddc6e4adbed2f24dfdd1584ea984f
Alexander Böhm <tools@boehm.work>
2018-11-14 14:01:05 +0100

    Add some more output for the user

--- gm_preview.py
+++ gm_preview.py
@@ -27,6 +27,9 @@ def main():
 def lowercase_file_extension(application_path):
     print()
     print("Überprüfe die Dateinamen nach Großbuchstaben:")
+    print()
+
+    count_files = 0
 
     for filename in os.listdir(application_path):
         if not os.path.splitext(filename)[1] == ".JPG":
@@ -35,13 +38,21 @@ def lowercase_file_extension(application_path):
         full_filename = os.path.join(application_path, filename)
         os.rename(full_filename, full_filename.lower())
 
-        print(full_filename.lower() + " wurde korrigiert")
+        print(filename + "\t wurde zu \t" + filename.lower() + "\t korrigiert")
 
-    # TODO: Output
+        count_files += 1
+
+    print()
+
+    if count_files > 0:
+        print("Insgesamt wurden " + str(count_files) + " Dateien umbenannt")
+    else:
+        print("Es mussten keine Dateien umbenannt werden.")
 
 
 def modify_images(application_path):
     output_dirs = [os.path.join(application_path, "..", "512"), os.path.join(application_path, "..", "150")]
+    count_files = 0
 
     print()
     print("--------------------------------------------------------------------")
@@ -52,12 +63,14 @@ def modify_images(application_path):
     for directory in output_dirs:
         if not os.path.isdir(directory):
             os.mkdir(directory)
-            print("Ordner " + os.path.realpath(directory) + " erstellt.")
+            print("Der Ordner " + os.path.realpath(directory) + " wurde erstellt.")
+        else:
+            print("Der Ordner " + os.path.realpath(directory) + " war bereits vorhanden.")
 
     print()
     print("--------------------------------------------------------------------")
     print()
-    print("Erstelle zu erst die Bilder für die Größe 512px:")
+    print("Erstelle die Bilder in der Größe \"512px\":")
     print()
 
     for filename in os.listdir(application_path):
@@ -73,13 +86,19 @@ def modify_images(application_path):
         img.resize("512x512>")
         img.write(os.path.join(output_dirs[0], filename))
 
-        print(filename + " in 512 erstellt.")
+        print(filename + "\t erfolgreich erstellt.")
 
+        count_files += 1
+
+    print()
+    print("Insgesamt wurden " + str(count_files) + " Bilder in der Größe \"512px\" erzeugt.")
     print()
     print("--------------------------------------------------------------------")
     print()
-    print("Erstelle nun die Bilder für die Größe 150px:")
+    print("Erstelle die Bilder in der Größe \"150px\":")
     print()
+
+    count_files = 0
 
     for filename in os.listdir(output_dirs[0]):
         if not filename.endswith(".jpg"):
@@ -89,12 +108,16 @@ def modify_images(application_path):
         img.resize("150x150>")
         img.write(os.path.join(output_dirs[1], filename))
 
-        print(filename + " in 150 erstellt.")
+        print(filename + "\t erfolgreich erstellt.")
 
+        count_files += 1
+
+    print()
+    print("Insgesamt wurden " + str(count_files) + " Bilder in der Größe \"150px\" erzeugt.")
     print()
     print("--------------------------------------------------------------------")
     print()
-    print("Alle Bilder wurden verarbeitet. Vielen Dank für die Nutzung!")
+    print("Die Verarbeitung ist beendet. Vielen Dank für die Nutzung dieses Tools!")
     print()
 
 
