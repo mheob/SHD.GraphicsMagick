7df9c07b85bc562bf41882d2e0cdb920e8bd33de
Alexander Böhm <tools@boehm.work>
2018-11-14 14:16:39 +0100

    Optimize imports and outputs. Also add output on shutting down the app

--- gm_preview.py
+++ gm_preview.py
@@ -1,5 +1,7 @@
-import os
 import sys
+
+from os import listdir, mkdir, path, rename
+from time import sleep
 
 import PythonMagick
 
@@ -9,10 +11,10 @@ def main():
     application_path = None
     if getattr(sys, 'frozen', False):
         # published executable
-        application_path = os.path.dirname(sys.executable)
+        application_path = path.dirname(sys.executable)
     elif __file__:
         # testing script
-        application_path = os.path.join(os.path.dirname(__file__), "testdata/_orig")
+        application_path = path.join(path.dirname(__file__), "testdata/_orig")
 
     if application_path is None:
         exit(1)
@@ -23,102 +25,106 @@ def main():
     # Modify the images
     modify_images(application_path)
 
+    # Out on shutting down the application
+    print()
+    print("\t--------------------------------------------------------------------")
+    print()
+    print("\tDie Verarbeitung ist beendet und dieses Fenster wird in Kürze geschlossen ...")
+    print("\tVielen Dank für die Nutzung dieses Tools!")
+    print()
+    sleep(3)
+
 
 def lowercase_file_extension(application_path):
     print()
-    print("Überprüfe die Dateinamen nach Großbuchstaben:")
+    print("\tÜberprüfe die Dateinamen nach Großbuchstaben:")
     print()
 
     count_files = 0
 
-    for filename in os.listdir(application_path):
-        if not os.path.splitext(filename)[1] == ".JPG":
+    for filename in listdir(application_path):
+        if not path.splitext(filename)[1] == ".JPG":
             continue
 
-        full_filename = os.path.join(application_path, filename)
-        os.rename(full_filename, full_filename.lower())
+        full_filename = path.join(application_path, filename)
+        rename(full_filename, full_filename.lower())
 
-        print(filename + "\t wurde zu \t" + filename.lower() + "\t korrigiert")
+        print("\t" + filename + "\t wurde zu \t" + filename.lower() + "\t korrigiert")
 
         count_files += 1
 
     print()
 
     if count_files > 0:
-        print("Insgesamt wurden " + str(count_files) + " Dateien umbenannt")
+        print("\tInsgesamt wurden " + str(count_files) + " Dateien umbenannt.")
     else:
-        print("Es mussten keine Dateien umbenannt werden.")
+        print("\tEs mussten keine Dateien umbenannt werden.")
 
 
 def modify_images(application_path):
-    output_dirs = [os.path.join(application_path, "..", "512"), os.path.join(application_path, "..", "150")]
+    output_dirs = [path.join(application_path, "..", "512"), path.join(application_path, "..", "150")]
     count_files = 0
 
     print()
-    print("--------------------------------------------------------------------")
+    print("\t--------------------------------------------------------------------")
     print()
-    print("Erstelle die benötigten Ordner, falls sie noch nicht vorhanden sind:")
+    print("\tErstelle die benötigten Ordner, falls sie noch nicht vorhanden sind:")
     print()
 
     for directory in output_dirs:
-        if not os.path.isdir(directory):
-            os.mkdir(directory)
-            print("Der Ordner " + os.path.realpath(directory) + " wurde erstellt.")
+        if not path.isdir(directory):
+            mkdir(directory)
+            print("\tDer Ordner " + path.realpath(directory) + " wurde erstellt.")
         else:
-            print("Der Ordner " + os.path.realpath(directory) + " war bereits vorhanden.")
+            print("\tDer Ordner " + path.realpath(directory) + " war bereits vorhanden.")
 
     print()
-    print("--------------------------------------------------------------------")
+    print("\t--------------------------------------------------------------------")
     print()
-    print("Erstelle die Bilder in der Größe \"512px\":")
+    print("\tErstelle die Bilder in der Größe \"512px\":")
     print()
 
-    for filename in os.listdir(application_path):
+    for filename in listdir(application_path):
         if not filename.endswith(".jpg"):
             continue
 
-        img = PythonMagick.Image(os.path.join(application_path, filename))
+        img = PythonMagick.Image(path.join(application_path, filename))
         # noinspection PyArgumentList
         img.strip()
         # noinspection PyArgumentList
         img.trim()
         img.quality(80)
         img.resize("512x512>")
-        img.write(os.path.join(output_dirs[0], filename))
+        img.write(path.join(output_dirs[0], filename))
 
-        print(filename + "\t erfolgreich erstellt.")
+        print("\t" + filename + "\t erfolgreich erstellt.")
 
         count_files += 1
 
     print()
-    print("Insgesamt wurden " + str(count_files) + " Bilder in der Größe \"512px\" erzeugt.")
+    print("\tInsgesamt wurden " + str(count_files) + " Bilder in der Größe \"512px\" erzeugt.")
     print()
-    print("--------------------------------------------------------------------")
+    print("\t--------------------------------------------------------------------")
     print()
-    print("Erstelle die Bilder in der Größe \"150px\":")
+    print("\tErstelle die Bilder in der Größe \"150px\":")
     print()
 
     count_files = 0
 
-    for filename in os.listdir(output_dirs[0]):
+    for filename in listdir(output_dirs[0]):
         if not filename.endswith(".jpg"):
             continue
 
-        img = PythonMagick.Image(os.path.join(output_dirs[0], filename))
+        img = PythonMagick.Image(path.join(output_dirs[0], filename))
         img.resize("150x150>")
-        img.write(os.path.join(output_dirs[1], filename))
+        img.write(path.join(output_dirs[1], filename))
 
-        print(filename + "\t erfolgreich erstellt.")
+        print("\t" + filename + "\t erfolgreich erstellt.")
 
         count_files += 1
 
     print()
-    print("Insgesamt wurden " + str(count_files) + " Bilder in der Größe \"150px\" erzeugt.")
-    print()
-    print("--------------------------------------------------------------------")
-    print()
-    print("Die Verarbeitung ist beendet. Vielen Dank für die Nutzung dieses Tools!")
-    print()
+    print("\tInsgesamt wurden " + str(count_files) + " Bilder in der Größe \"150px\" erzeugt.")
 
 
 if __name__ == '__main__':
