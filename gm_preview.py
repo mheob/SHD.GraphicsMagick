96594fc49b4803774e3463a951d46113ef352f91
Alexander Böhm <tools@boehm.work>
2018-11-14 09:34:24 +0100

    Include PythonMagick an made tool running correctly

--- gm_preview.py
+++ gm_preview.py
@@ -1,20 +1,49 @@
 import os
 
+import PythonMagick as pm
+
 
 def main():
-    # Current directory
-    directory = os.path.curdir + "/testdata/"
+    # Specify some variables
+    inputDir = os.path.curdir + "/testdata/"
 
     # Rename file extension to lowercase (JPG only)
-    lowercaseFileExtension(directory)
+    lowercaseFileExtension(inputDir)
+
+    # Modify the images
+    modifyImages(inputDir)
 
 
-def lowercaseFileExtension(directory):
-    for filename in os.listdir(directory):
+def lowercaseFileExtension(inputDir):
+    for filename in os.listdir(inputDir):
         if not os.path.splitext(filename)[1] == ".JPG":
             continue
-        fullFilename = directory + "/" + filename
+
+        fullFilename = inputDir + "/" + filename
         os.rename(fullFilename, fullFilename.lower())
+
+
+def modifyImages(inputDir):
+    outputDirs = [inputDir + "/../150", inputDir + "/../512"]
+
+    for directory in outputDirs:
+        if not os.path.isdir(directory):
+            os.mkdir(directory)
+
+    for filename in os.listdir(inputDir):
+        if not filename.endswith(".jpg"):
+            continue
+
+        for outputDir in outputDirs:
+            img = pm.Image(inputDir + filename)
+            img.strip()
+            img.trim()
+            img.quality(80)
+            img.resize("512x512>" if outputDir.endswith("512") else "150x150>")
+            img.write(outputDir + "/" + filename)
+
+            curSize = "150" if outputDir.endswith("150") else "512"
+            print(filename + " in " + curSize + " erstellt.")
 
 
 if __name__ == '__main__':
