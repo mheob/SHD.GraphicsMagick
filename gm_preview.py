c7a73a11576f8823ac32a58578b63115ec0cb3a3
Alexander Böhm <tools@boehm.work>
2018-11-14 11:45:11 +0100

    Add some output for the user

--- gm_preview.py
+++ gm_preview.py
@@ -15,6 +15,8 @@ def main():
 
 
 def lowercase_file_extension(input_dir):
+    print("Ändere die Dateinamen zu Kleinbuchstaben.")
+
     for filename in os.listdir(input_dir):
         if not os.path.splitext(filename)[1] == ".JPG":
             continue
@@ -26,9 +28,14 @@ def lowercase_file_extension(input_dir):
 def modify_images(input_dir):
     output_dirs = [input_dir + "../512/", input_dir + "../150/"]
 
+    print("Erstelle die benötigten Ordner, falls sie noch nicht vorhanden sind.")
+
     for directory in output_dirs:
         if not os.path.isdir(directory):
             os.mkdir(directory)
+            print("Ordner " + directory + " erstellt.")
+
+    print("Erstelle zu erst die Bilder für die Größe 512px.")
 
     for filename in os.listdir(input_dir):
         if not filename.endswith(".jpg"):
@@ -43,7 +50,9 @@ def modify_images(input_dir):
         img.resize("512x512>")
         img.write(output_dirs[0] + "/" + filename)
 
-        print(filename + " in " + "512" + " erstellt.")
+        print(filename + " in 512 erstellt.")
+
+    print("Erstelle nun die Bilder für die Größe 150px.")
 
     for filename in os.listdir(output_dirs[0]):
         if not filename.endswith(".jpg"):
@@ -53,7 +62,9 @@ def modify_images(input_dir):
         img.resize("150x150>")
         img.write(output_dirs[1] + "/" + filename)
 
-        print(filename + " in " + "150" + " erstellt.")
+        print(filename + " in 150 erstellt.")
+
+    print("Alle Bilder wurden verarbeitet.")
 
 
 if __name__ == '__main__':
